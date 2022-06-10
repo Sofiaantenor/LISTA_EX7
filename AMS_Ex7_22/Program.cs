a4353f8c20b17c57e93e9d7c1ea2a55e451be774
Sofiaantenor <90793659+Sofiaantenor@users.noreply.github.com>
2022-06-10 15:46:10 -0300

    Add files via upload

---
+++ AMS_Ex7_22/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AMS_Ex7_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1 = 10;
+            double nota2 = 7;
+            double nota3 = 9;
+            double nota4 = 8;
+            double media;
+
+            Console.WriteLine("");
+            Console.WriteLine("         ***CÁLCULO DA MÉDIA DE UM ALUNO***      ");
+
+            media = (nota1 + nota2 + nota3 + nota4) / 4;
+            Console.WriteLine("");
+
+            Console.WriteLine($"A média desse aluno é {media}");
+            Console.WriteLine("");
+
+            if (media > 7 )
+            {
+               Console.WriteLine("                  ALUNO APROVADO!");
+            } 
+            else
+            {
+               Console.WriteLine("                  ALUNO REPROVADO!");
+            }
+
+            Console.WriteLine("");
+        }
+    }
+}
